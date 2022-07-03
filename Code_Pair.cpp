55715e4d50a7ac497f62a93c07801134ffa7010a
venugopal <43407734+venu04@users.noreply.github.com>
2022-07-03 15:18:09 +0530

    Update Code_Pair.cpp

--- Code_Pair.cpp
+++ Code_Pair.cpp
@@ -6,22 +6,28 @@ output:
 [10,6]
 */
 
-#include <iostream>
-#include <vector>
-#include <unordered_set>
+#include<iostream>
+#include<vector>
+#include<unordered_set>
 
 using namespace std;
-
-int main()
-{
-    
-    vector<int> arr={10,5,2,3,-6,9,1};
-    vector<int> result;
-    unordered_set<int> s;
-    int sum=4,x,i;
-    for(i=0;i<arr.size();i++)
-    {
-        x=sum-arr[i];
+int main(){
+    vector <int> arr={10,5,2,3,-6,9,11};
+    vector <int> result;
+    unordered_set <int> s;
+    char ch;
+    int i;
+    cout<<"Enter Array \n";
+    ch=getchar();
+    while(ch!='\n'){
+        arr.push_back(ch-'0');
+        ch=getchar();
+    }
+    cout<<"Enter sum \n";
+    int sum;
+    cin>>sum;
+    for(i=0;i<arr.size();i++){
+        int x=sum-arr[i];
         if(s.find(x)!=s.end()){
             result.push_back(x);
             result.push_back(arr[i]);
@@ -29,9 +35,7 @@ int main()
         }
         s.insert(arr[i]);
     }
-    if(result.size()!=0)
-            cout<<result[0]<<","<<result[1];
-    else
-        cout<<"No such Pair";
+    cout<<"{"<<result[0]<<","<<result[1]<<"}";
     return 0;
+    
 }
