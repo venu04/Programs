ac16e1f62ce41a01b1a6221eef1c51b49fbc87aa
venugopal <43407734+venu04@users.noreply.github.com>
2022-07-11 10:31:01 +0530

    Create OddorEvenBitwise.cpp

---
+++ OddorEvenBitwise.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a=10;
+    if(a&1){
+        cout<<"ODD";
+    }
+    else
+    {
+        cout<<"EVEN";
+    }
+
+    return 0;
+}
